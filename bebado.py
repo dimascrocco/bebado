e70a37ceace78b9eeb295dacf2fdcee7173f1641
dimascrocco <dimas.crocco@gmail.com>
2015-08-30 01:17:41 -0300

    Drunk guy walks forward and backward... the wind doesn't help too much.

--- bebado.py
+++ bebado.py
@@ -1,9 +1,26 @@
 import random
+import collections
 
 print "Bebado..."
 
 MAX_PASSOS = 10
 passo = ['F', 'T']
+origem = 0
+
+p = 0.5
 
 print passo
+
+
+
+for i in range(MAX_PASSOS):
+  trupicada = passo[0] if random.random() < p else passo[1]
+  if trupicada == 'F':
+    origem += 1
+  else:
+    origem += -1
+
+print origem
+
+// TODO save results in a map with the result count and plot the results
 
