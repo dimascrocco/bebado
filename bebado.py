027740db242e3484822399021f6df53822c61b0a
Dimas Crocco <dimas.crocco@gmail.com>
2015-08-30 15:50:40 -0300

    Ouch... it's true.

--- bebado.py
+++ bebado.py
@@ -1,26 +1,46 @@
 import random
 import collections
 
-print "Bebado..."
+print "O Bebado e o Equilibrista..."
 
+M=100000
 MAX_PASSOS = 10
 passo = ['F', 'T']
-origem = 0
+resultado = {}
 
 p = 0.5
 
 print passo
 
+def caminhar():
+  origem = 0
+  passos = []
+  for i in range(MAX_PASSOS):
+    trupicada = passo[0] if random.random() < p else passo[1]
+    if trupicada == 'F':
+      origem += 1
+    else:
+      origem += -1
+    passos.append(trupicada)
 
+  if resultado.get(origem) == None: 
+    resultado[origem] = 0
 
-for i in range(MAX_PASSOS):
-  trupicada = passo[0] if random.random() < p else passo[1]
-  if trupicada == 'F':
-    origem += 1
-  else:
-    origem += -1
+  resultado[origem] += 1
 
-print origem
+  # print origem
+  # print passos
 
-// TODO save results in a map with the result count and plot the results
+for i in range(M):
+  caminhar()
+
+total = 0
+for k in sorted(resultado.keys()):
+  result =  resultado[k] / float(M)
+  print str(k) + " : " + str(result*100) + "%"
+  total += result
+
+print "Total = " + str(total)
+
+# TODO save results in a map with the result count and plot the results
 
